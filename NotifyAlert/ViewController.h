7e9d0a7fffdf2391bdbf653d32e3610d24c78cad
intent <intent@intents-iMac.local>
2015-01-29 15:53:35 -0800

    Initial Commit

---
+++ NotifyAlert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NotifyAlert
+//
+//  Created by intent on 29/01/15.
+//  Copyright (c) 2015 intent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NotifyAlert/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NotifyAlert
+//
+//  Created by intent on 29/01/15.
+//  Copyright (c) 2015 intent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
